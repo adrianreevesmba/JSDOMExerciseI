dee8fa585f2ca5cbfe1287418026b5ffb4301fb4
Adrian Reeves <AdrianReevesmba@gmail.com>
2023-12-05 17:48:15 -0500

    Committed initial DOM exercise

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// DOM
+// Access const elementById = document.getElementById('myElementId');
+// const elementByQuery = document.querySelector('.myClass');
+// const elementsByClass = document.getElementsByClassName('myClass'); //
+
+// const myElement = document.getElementById('myElementId');
+// myElement.textContent = 'New Text Content';
+
+// const newParagraph = document.createElement('p');
+// newParagraph.textContent = 'This is a new paragraph.';
+// document.body.appendChild(newParagraph);
+// const paragraphElement = document.getElementById('myParagraph');
+// const parentElement = paragraphElement.parentNode;
+// const nextElement = paragraphElement.nextSibling;
+// const previousElement = paragraphElement.previousSibling;
+
+
+// Assignment
+// Get the header element/
+
+let headertag = document.querySelector("header")
+// Get all the section elements
+let sections = document.getElementsByClassName("col")[0].children
+// Get the section element with class="current"
+let current = document.getElementsByClassName("current")[0]
+// Get the section that comes after the current section
+
+// Get the h2 node from the section before the 'current' section
+
+// Get the div that contains the section that has an h2 with a class of 'highlight'
+
+// Get all the sections that contain an H2 (using a single statement);
